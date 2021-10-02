a0ce99a59198997982cb77498d01fd6503ea759b
John Daly <jcdaly97@gmail.com>
2021-10-01 18:30:48 -0700

    saving changes and switching to chat branch

--- src/components/chat/ChatForm.js
+++ src/components/chat/ChatForm.js
@@ -1,13 +1,31 @@
 import React, { useState } from 'react';
 
 function ChatForm(props) {
+	const [newMessage, setNewMessage] = useState('');
+
+	const handleSubmit = async (e) => {
+		//get the code from the current key press
+		const code = e.keyCode || e.which
+
+		//if the code is 13 (enter) then submit the message
+		if (code === 13) {
+			console.log(newMessage)
+			setNewMessage('')
+		}
+
+	}
+
 	return (
 		<>
 			<label>
 				Say Something:
-				<input type='text' />
+				<input
+					id='chat-input'
+					onChange={(e) => setNewMessage(e.target.value)}
+					onKeyPress={handleSubmit}
+					value={newMessage}
+				/>
 			</label>
-			<input type="submit" value='Submit' />
 		</>
 	)
 }
